94e7e83ffd577f8e70e9a07312bae95ae89cd0c1
mbgrageda <marco.angelo127@gmail.com>
2026-01-23 22:26:00 +0800

    sana tama

--- Technical1_Website/scirpt.js
+++ Technical1_Website/scirpt.js
@@ -1,9 +1,17 @@
-
 function productClicked(productName) {
     alert("You clicked on " + productName);
 }
 
-
 function submitForm() {
     alert("Thank you for your message!");
+}
+
+function loginUser() {
+    alert("Login successful (placeholder)");
+    window.location.href = "profile.html";
+}
+
+function signupUser() {
+    alert("Account created successfully (placeholder)");
+    window.location.href = "login.html";
 }
