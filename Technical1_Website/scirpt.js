781eb9b335c2e6f21077a8f79d049d7d1b1f2d16
mbgrageda <marco.angelo127@gmail.com>
2026-01-26 15:39:40 +0800

    addded

--- Technical1_Website/scirpt.js
+++ Technical1_Website/scirpt.js
@@ -10,3 +10,48 @@ function loginUser() {
     alert("Login successful (placeholder)");
     window.location.href = "profile.html";
 }
+
+let cart = [];
+let total = 0;
+let payment = "";
+let delivery = "";
+
+function addToCart(product, price) {
+    cart.push(product + " - ₱" + price);
+    total += price;
+    alert(product + " added to cart");
+}
+
+function loadCart() {
+    let cartDiv = document.getElementById("cartItems");
+    let totalSpan = document.getElementById("totalPrice");
+
+    if (!cartDiv) return;
+
+    cartDiv.innerHTML = "";
+
+    for (let i = 0; i < cart.length; i++) {
+        cartDiv.innerHTML += "<p>" + cart[i] + "</p>";
+    }
+
+    totalSpan.innerHTML = total;
+}
+
+function placeOrder() {
+    payment = document.getElementById("paymentMethod").value;
+    delivery = document.getElementById("deliveryOption").value;
+    window.location.href = "confirmation.html";
+}
+
+function loadConfirmation() {
+    if (!document.getElementById("showPayment")) return;
+
+    document.getElementById("showPayment").innerHTML = payment;
+    document.getElementById("showDelivery").innerHTML = delivery;
+    document.getElementById("showTotal").innerHTML = total;
+}
+
+window.onload = function () {
+    loadCart();
+    loadConfirmation();
+};
